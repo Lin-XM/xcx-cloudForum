0c34744b2c171eb2590cb64bd7503c9605f804eb
LXM <2602863701@qq.com>
2023-07-05 08:41:57 +0800

    init

---
+++ pages/index/index.vue
@@ -0,0 +1,24 @@
+<template>
+	<view class="home">
+		主页
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss";>
+
+</style>
